6fb0472bf6d606b53ad64e9c48b2547b8b8602fc
Dronip <102549861+Dronip@users.noreply.github.com>
2023-06-19 17:51:28 +0300

    Kanapel_Project_Camp
    Kanapel_Project_Camp

---
+++ Kanapel_Project_Camp/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    int fish = 1345678; 
+    int car = 1345;
+    int  
+
+}
